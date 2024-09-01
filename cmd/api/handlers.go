684b4238f2c170c166a65ad4aad4515fd3891485
Dubass83 <makssych@gmail.com>
2024-09-01 19:40:48 +0300

    feat: add helpers functions for micro services

--- cmd/api/handlers.go
+++ cmd/api/handlers.go
@@ -1,16 +1,8 @@
 package api
 
 import (
-	"encoding/json"
-	"log"
 	"net/http"
 )
-
-type jsonResponse struct {
-	Error   bool   `json:"error"`
-	Massage string `json:"massage"`
-	Data    any    `json:"data,omitempty"`
-}
 
 // Broker api Handler
 func Broker(w http.ResponseWriter, r *http.Request) {
@@ -19,12 +11,5 @@ func Broker(w http.ResponseWriter, r *http.Request) {
 		Massage: "Hello from Broker!",
 	}
 
-	out, err := json.MarshalIndent(payload, "", "\t ")
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	w.Header().Set("Content-Type", "application/json")
-	w.WriteHeader(http.StatusAccepted)
-	w.Write([]byte(out))
+	_ = writeJSON(w, http.StatusAccepted, payload)
 }
